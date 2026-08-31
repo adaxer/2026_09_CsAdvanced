7eb122f81ba95a9d0d6190f641294b206ab24b3e
Andreas Daxer <info@andreas-daxer.de>
2026-08-31 16:34:41 +0200

    DelegatesAndEvents

--- M-004-CS-Fortgeschritten Delegates/DelegateDemo/DelegateDemo/Program.cs
+++ M-004-CS-Fortgeschritten Delegates/DelegateDemo/DelegateDemo/Program.cs
@@ -4,8 +4,36 @@ internal class Program
 {
     public delegate string MyDelegate(int value);
 
+
     static void Main(string[] args)
     {
+        // Alles aus Kap 05
+        EventSource.MyEventAsAction = (sender, e) => Console.WriteLine("MyEventAsAction triggered");
+
+        EventHandler writeMessage = (sender, e) => Console.WriteLine("MyEventAsAction triggered again");
+        EventSource.MyEventAsAction += writeMessage;
+
+        EventSource.MyEventAsAction.Invoke(null, EventArgs.Empty);
+        EventSource.MyEventAsAction -= writeMessage;
+
+        EventSource.MyEventAsAction(null, EventArgs.Empty);
+
+        // EventSource.MyEventAsEvent = (sender, e) => Console.WriteLine("MyEventAsEvent triggered"); // geht nicht, da MyEventAsEvent ein Event ist und nicht direkt zugewiesen werden kann
+        EventSource.MyEventAsEvent += (sender, e) => Console.WriteLine("MyEventAsEvent triggered");
+        // EventSource.MyEventAsEvent.Invoke(null, EventArgs.Empty); // geht nicht, da MyEventAsEvent ein Event ist und nicht direkt aufgerufen werden kann
+
+        EventSource.MyActionAsEvent += () => Console.WriteLine("MyActionAsEvent triggered");
+
+        EventSource.TriggerEvents();
+
+        List<int> numbers = Enumerable.Range(0, 100).ToList();
+
+        ForEach<int>(numbers, i => Console.WriteLine(i));
+        var resultList = ForEachResult<int, bool>(numbers, i => i % 2 == 0);
+
+        Console.WriteLine(resultList.Where(b => true).Count());
+        Console.WriteLine(resultList.Where(b => false).Count());
+
         Func<int, string> HalfIt = i =>
         {
             string result = $"Die Hälfte von {i} ist: {(i / 2)}";
@@ -13,12 +41,30 @@ internal class Program
             return result;
         };
         MyDelegate del = PrintInt;
-        del += i=> (2*i).ToString();
+        del += i => (2 * i).ToString();
         del += new MyDelegate(HalfIt);
         del(43);
 
         del = PrintInt;
         del(44);
+    }
+
+    static void ForEach<T>(List<T> collection, Action<T> callBack)
+    {
+        for (int i = 0; i < collection.Count; i++)
+        {
+            callBack(collection[i]);
+        }
+    }
+
+    static List<R> ForEachResult<T, R>(List<T> collection, Func<T, R> callBack)
+    {
+        List<R> results = new List<R>();
+        for (int i = 0; i < collection.Count; i++)
+        {
+            var value = callBack(collection[i]);
+        }
+        return results;
     }
 
     private static string PrintInt(int value)
@@ -30,3 +76,21 @@ internal class Program
 
 
 }
+
+public static class EventSource
+{
+    public static EventHandler MyEventAsAction = (sender, e) => { };
+
+    // Aus .Net-History
+    public static event EventHandler MyEventAsEvent;
+
+    public static event Action MyActionAsEvent;
+
+    internal static void TriggerEvents()
+    {
+        MyEventAsAction(null, EventArgs.Empty);
+        MyEventAsEvent?.Invoke(null, EventArgs.Empty);
+        MyActionAsEvent?.Invoke();
+    }
+}
+
