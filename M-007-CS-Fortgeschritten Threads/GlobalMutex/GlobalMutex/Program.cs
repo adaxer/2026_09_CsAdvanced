29aedde1fc4659abf3bbc3b0fa52008fcc8fa707
Andreas Daxer <info@andreas-daxer.de>
2026-09-01 11:55:39 +0200

    Threads

---
+++ M-007-CS-Fortgeschritten Threads/GlobalMutex/GlobalMutex/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GlobalMutex;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Mutex mutex = new Mutex(false, "MeinMutex");
+        for (int i = 0; i < 100; i++)
+        {
+            mutex.WaitOne();
+            Thread.Sleep(300);
+            Console.WriteLine(i);
+            mutex.ReleaseMutex();
+        }
+        Console.ReadKey();
+
+    }
+
+}
---
+++ M-007-CS-Fortgeschritten Threads/ThreadingDemo/ThreadingDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ThreadingDemo;
+
+internal class Program
+{
+    static int externalCounter = 0;
+    private static Thread t1;
+    private static Thread t2;
+
+    static void Main(string[] args)
+    {
+        t1 = new Thread(Count);
+        t1.IsBackground = true;
+        t1.Start();
+
+        t2 = new Thread(Count);
+        t2.Start();
+        Console.ReadLine();
+    }
+
+    private static void Count(object? obj)
+    {
+        int internalCounter = 0;
+        for (int i = 0; i < 1000000; i++)
+        {
+            lock (typeof(Program))
+            {
+                internalCounter++;
+                externalCounter++;
+                Console.WriteLine($"Id: {Thread.CurrentThread.ManagedThreadId}, Int: {internalCounter}, Ext: {externalCounter}");
+            }
+            Thread.Sleep(100);
+            var otherThread = Thread.CurrentThread == t1 ? t2 : t1;
+            otherThread.Join();
+        }
+    }
+}
